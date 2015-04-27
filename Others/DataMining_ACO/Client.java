8fe6fee4592988e5655d87b1c8eab974b58f4e91
linyiqun <675250079@qq.com>
2015-04-27 22:21:59 +0800

    算法场景测试类
    算法场景测试类

---
+++ Others/DataMining_ACO/Client.java
@@ -0,0 +1,29 @@
+package DataMining_ACO;
+
+/**
+ * 蚁群算法测试类
+ * @author lyq
+ *
+ */
+public class Client {
+	public static void main(String[] args){
+		//测试数据
+		String filePath = "C:\\Users\\lyq\\Desktop\\icon\\input.txt";
+		//蚂蚁数量
+		int antNum;
+		//控制参数
+		double alpha;
+		double beita;
+		double p;
+		double Q;
+		
+		antNum = 3;
+		alpha = 0.5;
+		beita = 1;
+		p = 0.5;
+		Q = 5;
+		
+		ACOTool tool = new ACOTool(filePath, antNum, alpha, beita, p, Q);
+		tool.antStartSearching();
+	}
+}
