9e16a9842f026bb70a6e0282bcbb8be9b4fcd820
linyiqun <675250079@qq.com>
2015-04-28 21:16:45 +0800

    蚂蚁类添加当前路径包含子路径方法
    蚂蚁类添加当前路径包含子路径方法

--- Others/DataMining_ACO/Ant.java
+++ Others/DataMining_ACO/Ant.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  * @author lyq
  * 
  */
-public class Ant implements Comparable<Ant>{
-	//蚂蚁当前所在城市
+public class Ant implements Comparable<Ant> {
+	// 蚂蚁当前所在城市
 	String currentPos;
 	// 蚂蚁遍历完回到原点所用的总距离
 	Double sumDistance;
@@ -51,39 +51,70 @@ public class Ant implements Comparable<Ant>{
 
 		return sumDistance;
 	}
-	
+
 	/**
 	 * 蚂蚁选择前往下一个城市
+	 * 
 	 * @param city
-	 * 所选的城市
+	 *            所选的城市
 	 */
-	public void goToNextCity(String city){
+	public void goToNextCity(String city) {
 		this.currentPath.add(city);
 		this.currentPos = city;
 		this.nonVisitedCitys.remove(city);
 		this.visitedCitys.add(city);
 	}
-	
+
 	/**
 	 * 判断蚂蚁是否已经又重新回到起点
+	 * 
 	 * @return
 	 */
-	public boolean isBack(){
+	public boolean isBack() {
 		boolean isBack = false;
 		String startPos;
 		String endPos;
-		
-		if(currentPath.size() == 0){
+
+		if (currentPath.size() == 0) {
 			return isBack;
 		}
-		
+
 		startPos = currentPath.get(0);
-		endPos = currentPath.get(currentPath.size()-1);
-		if(currentPath.size() > 1 && startPos.equals(endPos)){
+		endPos = currentPath.get(currentPath.size() - 1);
+		if (currentPath.size() > 1 && startPos.equals(endPos)) {
 			isBack = true;
 		}
-		
+
 		return isBack;
+	}
+
+	/**
+	 * 判断蚂蚁在本次的走过的路径中是否包含从城市i到城市j
+	 * 
+	 * @param cityI
+	 *            城市I
+	 * @param cityJ
+	 *            城市J
+	 * @return
+	 */
+	public boolean pathContained(String cityI, String cityJ) {
+		String lastCity;
+		String currentCity;
+		boolean isContained = false;
+
+		for (int i = 0; i < currentPath.size() - 1; i++) {
+			lastCity = currentPath.get(i);
+			currentCity = currentPath.get(i + 1);
+
+			// 如果某一段路径的始末位置一致，则认为有经过此城市
+			if ((lastCity.equals(cityI) && currentCity.equals(cityJ))
+					|| (lastCity.equals(cityJ) && currentCity.equals(cityI))) {
+				isContained = true;
+				break;
+			}
+		}
+
+		return isContained;
 	}
 
 	@Override
