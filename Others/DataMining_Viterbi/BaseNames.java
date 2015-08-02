20302fd5aa232c454811b81f211457ae7553dbdc
linyiqun <675250079@qq.com>
2015-08-02 14:07:21 +0800

    维特比算法宏定义变量类
    维特比算法宏定义变量类

---
+++ Others/DataMining_Viterbi/BaseNames.java
@@ -0,0 +1,24 @@
+package DataMining_Viterbi;
+
+/**
+ * 基本变量定义类
+ * @author lyq
+ *
+ */
+public class BaseNames {
+	//日期天数下标
+	public static final int DAY1 = 0;
+	public static final int DAY2 = 1;
+	public static final int DAY3 = 2;
+	
+	//天气属性类别
+	public static final int WEATHER_SUNNY = 0;
+	public static final int WEATHER_CLOUDY = 1;
+	public static final int WEATHER_RAINY = 2;
+	
+	//湿度属性类别
+	public static final int HUMIDITY_DRY = 0;
+	public static final int HUMIDITY_DRYISH = 1;
+	public static final int HUMIDITY_DAMP = 1;
+	public static final int HUMIDITY_SOGGY = 1;
+}
